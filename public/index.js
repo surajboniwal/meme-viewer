55672172e43a8cd5423b28a6e43214726fdfc4e8
Suraj Boniwal <surajboniwal18@gmail.com>
2020-11-09 12:58:01 +0530

    Added copy url

--- public/index.js
+++ public/index.js
@@ -1,15 +1,24 @@
 const Http = new XMLHttpRequest()
 const url = "https://meme-api.herokuapp.com/gimme"
 const memeImage = document.querySelector('#meme')
+var image
 
 document.body.onkeyup = function(e){
     if(e.keyCode == 32){
         Http.open("GET", url)
         Http.send()        
     }
+    if(e.keyCode == 67){
+        var tempInput = document.createElement("input")
+        tempInput.value = JSON.parse(Http.responseText).url
+        document.body.appendChild(tempInput)
+        tempInput.select()
+        document.execCommand("copy")
+        document.body.removeChild(tempInput)   
+    }
 }
 
 Http.onreadystatechange = function(e){
-    memeImage.src = JSON.parse(Http.responseText).url
-    console.log(JSON.parse(Http.responseText).url)
+    image = JSON.parse(Http.responseText).url
+    memeImage.src = image
 }
